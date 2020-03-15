1fe3e2b433d515f5ffc9597d9ebe46121253cbe8
Méloé <meloscavezzoni@gmail.com>
2020-03-15 16:00:48 +0100

    Navbar: beginning scroll animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,22 @@
-// Menu
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+(() => {
+  // MENU
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
-// const links = document.querySelectorAll(".nav-links li");
+  // const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  hamburger.classList.toggle("open");
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    hamburger.classList.toggle("open");
 
-  // links.forEach(link => {
-  //   link.classList.toggle("fade");
-  // });
-});
+    // links.forEach(link => {
+    //   link.classList.toggle("fade");
+    // });
+  });
+
+  // SCROLL
+  window.addEventListener("scroll", () => {
+    let header = document.querySelector(".header");
+    header.classList.toggle("scroll-change", window.scrollY > 0);
+  });
+})();
