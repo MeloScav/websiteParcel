20f05f66e8f7059d0c7b6c37b31a398e444bac8a
Méloé <meloscavezzoni@gmail.com>
2020-03-19 11:48:17 +0100

    Navbar: scroll animation (logo) + responsive

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,14 @@
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
 
-  // const links = document.querySelectorAll(".nav-links li");
-
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     hamburger.classList.toggle("open");
-
-    // links.forEach(link => {
-    //   link.classList.toggle("fade");
-    // });
   });
 
   // SCROLL
   window.addEventListener("scroll", () => {
-    let header = document.querySelector(".header");
+    const header = document.querySelector(".header");
     header.classList.toggle("scroll-change", window.scrollY > 0);
   });
 })();
