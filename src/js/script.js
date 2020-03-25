717da1d8ab5eee23743bb23b16af86d60fbd2aeb
Méloé <meloscavezzoni@gmail.com>
2020-03-25 14:57:42 +0100

    Debug links

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 (() => {
   // MENU
+  const body = document.body;
+  const html = body.parentElement;
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
 
   hamburger.addEventListener("click", () => {
+    html.classList.toggle("no-scroll");
+    body.classList.toggle("no-scroll");
     navLinks.classList.toggle("open");
     hamburger.classList.toggle("open");
   });
